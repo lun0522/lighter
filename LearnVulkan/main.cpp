a9a0cb6f6571cc16012c18d7dde5c4166e0ddaba
lun0522 <pl557@cornell.edu>
2018-11-28 17:03:50 -0500

    Base code & Instance

--- LearnVulkan/main.cpp
+++ LearnVulkan/main.cpp
@@ -6,28 +6,85 @@
 //  Copyright © 2018 Pujun Lun. All rights reserved.
 //
 
+#include <stdlib.h>
 #include <iostream>
 
-#define GLFW_INCLUDE_VULKAN
-#include <vulkan/vulkan.hpp>
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan.hpp>
 
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    glfwInit();
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Learn Vulkan", nullptr, nullptr);
+class VulkanApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+private:
+    GLFWwindow *window;
+    VkInstance instance;
     
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-    cout << extensionCount << " extensions supported" << endl;
-    
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    void initWindow(int width = 800, int height = 600) {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        window = glfwCreateWindow(width, height, "Learn Vulkan", nullptr, nullptr);
     }
     
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    return 0;
+    void initVulkan() {
+        createInstance();
+    }
+    
+    void createInstance() {
+        if (glfwVulkanSupported() == GL_FALSE)
+            throw runtime_error("Vulkan not supported");
+        
+        uint32_t glfwExtensionCount;
+        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        
+        // optional. might be useful for the driver to optimize for some graphics engine
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Learn Vulkan";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_VERSION_1_0;
+        
+        // non-optional. tell the driver which global extensions and validation layers to use
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+        
+        // the second parameter is a pointer to custom allocator callbacks
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+            throw runtime_error("Failed to create instance");
+    }
+    
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+    
+    void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    VulkanApplication app;
+    try {
+        app.run();
+    } catch (const exception& e) {
+        cerr << "Error: " << e.what() << endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
