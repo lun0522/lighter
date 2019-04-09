22aad751c69774c6a89d0624c97c660bceed8e7c
lun0522 <pl557@cornell.edu>
2019-04-08 22:56:58 -0700

    Working on text

--- jessie_steamer/wrapper/vulkan/text.h
+++ jessie_steamer/wrapper/vulkan/text.h
@@ -8,6 +8,21 @@
 #ifndef JESSIE_STEAMER_WRAPPER_VULKAN_TEXT_H
 #define JESSIE_STEAMER_WRAPPER_VULKAN_TEXT_H
 
+#include <string>
 
+#include "third_party/glm/glm.hpp"
+
+namespace wrapper {
+namespace vulkan {
+
+struct Character {
+  glm::ivec2 size;
+  glm::ivec2 bearing;
+  unsigned int advance;
+};
+
+
+} /* namespace vulkan */
+} /* namespace wrapper */
 
 #endif //JESSIE_STEAMER_WRAPPER_VULKAN_TEXT_H
